b02362e617122fc4e5832f299360e2010b1b4f48
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2024-03-22 12:25:18 -0300

    revert of last commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path';
-import { dirname } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(dirname, 'src'),
-    },
-  },
 });
